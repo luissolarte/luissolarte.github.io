96864b897c91a17533e59cc40da66994d37234ca
Luis Solarte <solarte@outlook.com>
2023-08-10 00:13:21 -0500

    phenoagro v 1.3 alert

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "41e16a0755acdad353b3c4b8373b0940",
-"index.html": "e5842460ed74d296a8a9280b0c27f23d",
-"/": "e5842460ed74d296a8a9280b0c27f23d",
-"main.dart.js": "563a0c7c1bb4022dab8e7c09d22389ad",
+"index.html": "8c40a5f40b801e08711fe797d08578a8",
+"/": "8c40a5f40b801e08711fe797d08578a8",
+"main.dart.js": "2ac7efe6546941164fd094f37ef2313e",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
